a822ec165d4aed710c213535b441979ad52df391
Majbaj50 <djelveh.m@googlemail.com>
2025-05-25 21:43:23 +0700

    update icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+      <link rel="apple-touch-icon" sizes="180x180" href="/logo.svg" />
+        <link rel="icon" sizes="32x32" href="/logo.svg" />
+        <link rel="icon" sizes="16x16" href="/logo.svg" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
